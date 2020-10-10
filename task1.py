8f2b705123c6efd59783dce600aebb61ba7c51f8
nikibaru <nikibaruhosh@gmail.com>
2020-10-10 23:28:23 +0200

    add the code

---
+++ task1.py
@@ -0,0 +1,6 @@
+#создаем список
+L = ['string', 123.45, (6, 2, 8, 'm'), ['asdf', 168, 48], {'key_1': 156, 'key_2': 'dfg'}, set([12, 'sdfg']), True, None]
+#проверка и вывод
+for i in L:
+    print(type(i))
+
