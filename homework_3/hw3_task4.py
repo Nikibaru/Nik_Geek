5bb9d0e9847bc1a3b2d39ed790140b8019d41258
nikibaru <nikibaruhosh@gmail.com>
2020-10-14 00:52:14 +0200

    added func power_var3

--- homework_3/hw3_task4.py
+++ homework_3/hw3_task4.py
@@ -12,5 +12,15 @@ def power_var2(x, y):
     else:
         return(0)
 
+def power_var3(x, y):
+    counter = 0
+    multiple = 1 
+    while (counter < abs(y)):
+        multiple = multiple * x
+        counter += 1
+    if (y < 0):
+        return round(1/multiple, 8)
+    else: 
+        return round(multiple, 8)
 #test
-print(power_var1(3.45, -6) , power_var2(3.45, -6))
+print(power_var1(3.45, -6) , power_var2(3.45, -6), power_var3(3.45, -6))
