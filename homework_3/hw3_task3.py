4934100877cd6c483af069358fd0166a68196498
nikibaru <nikibaruhosh@gmail.com>
2020-10-13 23:53:22 +0200

    the code was improved: added my_func

--- homework_3/hw3_task3.py
+++ homework_3/hw3_task3.py
@@ -1,4 +1,5 @@
-def my_func(arg1, arg2, arg3):
+# my primitive attempt to write a func for comparison, but it works fine only for positive numbers
+def my_func1(arg1, arg2, arg3):
     #exception of dividing by zero
     if (arg1*arg2*arg3 == 0):
         return (arg1+arg2+arg3)
@@ -11,6 +12,10 @@ def my_func(arg1, arg2, arg3):
         else:
             return(arg2+arg3)
 
+# right solution
+def my_func(arg1, arg2, arg3):
+    return max(arg1, arg2)+max(arg2, arg3)
+
 #test
-a, b, c = 15,0,13
+a, b, c = 15,0,-13
 print(my_func(a,b,c))
